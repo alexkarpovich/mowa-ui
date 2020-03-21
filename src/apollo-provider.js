d7f704779827c894a2f6de756447418abb3221f5
Aliaksandr Karpovich <alexsure.k@gmail.com>
2020-03-21 22:00:05 +0300

    graphql api url change

--- src/apollo-provider.js
+++ src/apollo-provider.js
@@ -10,7 +10,7 @@ import typeDefs from './graphql/typeDefs';
 import resolvers from 'graphql/resolvers';
 
 const httpLink = createHttpLink({
-  uri: `http://${process.env.REACT_APP_HOST_IP}:4000/api`
+  uri: `https://api.mowa.local/graphql`
 });
 
 const authLink = setContext(() => {
